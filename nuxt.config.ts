068bbd021507c1b5b4ea9e69f02b764e50aedf6e
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-06-08 16:25:42 +0300

    Add Naive UI

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["nuxtjs-naive-ui"],
   ssr: false,
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
