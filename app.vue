b07965944824365d1f0f2706fbf23b145292ff18
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-06-08 17:07:56 +0300

    Add firebase js sdk

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
-    <h1 class="h-5 flex lg:bg-blue-500 border bg-red-500 flex-col" >Hello World</h1>
+    <h1 class="h-5 flex lg:bg-blue-500 border bg-red-500 flex-col">
+      Hello World
+    </h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["nuxtjs-naive-ui"],
+  modules: ["nuxtjs-naive-ui", "nuxt-vuefire"],
   ssr: false,
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
@@ -8,6 +8,19 @@ export default defineNuxtConfig({
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
+    },
+  },
+  vuefire: {
+    auth: {
+      enabled: true,
+    },
+    config: {
+      apiKey: "AIzaSyD9fEYHYqioldLk1rFaoaiZR59oynavc2k",
+      authDomain: "ms-task-26752.firebaseapp.com",
+      projectId: "ms-task-26752",
+      storageBucket: "ms-task-26752.appspot.com",
+      messagingSenderId: "534829434319",
+      appId: "1:534829434319:web:72a480c61d890948ea7eba",
     },
   },
 });
