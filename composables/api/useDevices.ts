cd64e6848decff14db9d6053cdaec8f7e5b93033
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-06-10 08:00:34 +0300

    Add useDevices composable

---
+++ composables/api/useDevices.ts
@@ -0,0 +1,82 @@
+import type { DeviceResponse } from "~/types/api";
+import { useMessage } from "naive-ui";
+import usePocketBase from "~/composables/server/usePocketbase";
+import { DeviceAvailabilityOptions } from "~/types/enums";
+
+export default function () {
+  const client = usePocketBase();
+  const message = useMessage();
+
+  const devices = useState<DeviceResponse[]>("devices", () => []);
+
+  // Create a new device
+  const createDevice = async (name: string, maintenanceTime?: string) => {
+    try {
+      await client.collection("devices").create({
+        name,
+        availability: DeviceAvailabilityOptions.ONLINE,
+        maintenanceTime: maintenanceTime || "",
+      });
+
+      message.success("Device created successfully");
+    } catch (error) {
+      message.error("Failed to create device");
+    }
+  };
+
+  // Fetch all devices
+  const fetchDevices = async () => {
+    try {
+      const res = await client
+        .collection("devices")
+        .getFullList<DeviceResponse>(50, {
+          expand: "alerts(device)",
+        });
+      devices.value = res;
+    } catch (error) {
+      console.error("Failed to get devices");
+    }
+  };
+
+  // Update a device
+  const updateDevice = async (deviceId: string, data: Object) => {
+    try {
+      await client.collection("devices").update<DeviceResponse>(deviceId, data);
+
+      message.success("Device updated successfully");
+    } catch (error) {
+      message.error("Failed to update device");
+    }
+  };
+
+  // listen to changes in devices
+  const subscribeDevice = async () => {
+    try {
+      await client.collection("devices").subscribe("*", ({ record }) => {
+        const index = devices.value.findIndex((d) => d.id === record.id);
+        if (index !== -1) {
+          devices.value[index] = record as DeviceResponse;
+        }
+      });
+    } catch (error) {
+      message.error("Failed to subscribe to devices");
+    }
+  };
+
+  const unsubscribeDevice = async () => {
+    try {
+      await client.collection("devices").unsubscribe("*");
+    } catch (error) {
+      message.error("Failed to unsubscribe to devices");
+    }
+  };
+
+  return {
+    devices,
+    createDevice,
+    fetchDevices,
+    updateDevice,
+    subscribeDevice,
+    unsubscribeDevice,
+  };
+}
