975bdfb02aef5050ad9acb81dfcae41266d00ae3
ahmedd-mahmoud <87312437+ahmedd-mahmoud@users.noreply.github.com>
2024-06-10 08:00:09 +0300

    Create all api types

---
+++ types/api.d.ts
@@ -0,0 +1,86 @@
+import {
+  DeviceAvailabilityOptions,
+  AlertSeverityOptions,
+  AlertStatusOptions,
+  TicketPriorityOptions,
+  TicketStatusOptions,
+} from "./enums";
+
+export enum Collections {
+  USERS = "users",
+  DEVICES = "devices",
+  ALERTS = "alerts",
+  TICKETS = "tickets",
+}
+
+// Alias types for improved usability
+export type IsoDateString = string;
+export type RecordIdString = string;
+
+// System fields
+export type BaseSystemFields<E> = {
+  id: RecordIdString;
+  created: IsoDateString;
+  updated: IsoDateString;
+  collectionId: string;
+  collectionName: Collections | string;
+  expand?: E;
+};
+
+export type AuthSystemFields = {
+  email: string;
+  emailVisibility: boolean;
+  username: string;
+  verified: boolean;
+} & BaseSystemFields;
+
+// Record types for each collection
+export type UserRecord = {
+  name?: string;
+  avatar?: string;
+};
+
+export type DeviceRecord = {
+  name: RecordIdString;
+  availability: DeviceAvailabilityOptions;
+  maintenanceTime: string;
+};
+
+export type AlertRecord = {
+  device: RecordIdString;
+  details: string;
+  severity: AlertSeverityOptions;
+  status: AlertStatusOptions;
+};
+
+export type TicketRecord = {
+  title: string;
+  priority: TicketPriorityOptions;
+  activityStatus: TicketStatusOptions;
+  assignedTechnician: string;
+};
+
+export type DeviceExpand = {
+  "alerts(device)": AlertResponse[];
+};
+
+export type AlertExpand = {
+  device: DeviceResponse;
+};
+
+// Response types include system fields and match responses from the PocketBase API
+export type DeviceResponse = DeviceRecord & BaseSystemFields<DeviceExpand>;
+
+export type AlertResponse = AlertRecord & BaseSystemFields<AlertExpand>;
+
+export type TicketResponse<E = DefaultExpand> = TicketRecord &
+  BaseSystemFields<E>;
+
+export type UsersResponse<E = DefaultExpand> = UserRecord & AuthSystemFields<E>;
+
+export type CollectionRecords = {
+  users: UserRecord;
+  devices: DeviceRecord;
+  alerts: AlertRecord;
+  tickets: TicketRecord;
+};
---
+++ types/enums.ts
@@ -0,0 +1,29 @@
+export enum DeviceAvailabilityOptions {
+  ONLINE = "online",
+  OFFLINE = "offline",
+  UNDER_MAINTENANCE = "under_maintenance",
+}
+
+export enum AlertSeverityOptions {
+  INFO = "info",
+  WARNING = "warning",
+  CRITICAL = "critical",
+}
+
+export enum AlertStatusOptions {
+  PENDING = "pending",
+  RESOLVED = "resolved",
+  IN_PROCESS = "in_process",
+}
+
+export enum TicketPriorityOptions {
+  LOW = "low",
+  HIGH = "high",
+  CRITICAL = "critical",
+}
+
+export enum TicketStatusOptions {
+  READ = "read",
+  UNREAD = "unread",
+  AWAITING = "awaiting",
+}
